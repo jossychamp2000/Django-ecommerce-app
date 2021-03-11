eeb016df6ab8d1cac22d9edcddcbc89508ea35fe
janejane <janetjanedchic@yahoo.com>
2021-03-11 17:57:24 -0500

    Created: product-list view & product_list.html

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 
 User = get_user_model()
