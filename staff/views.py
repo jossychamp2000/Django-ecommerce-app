6dd889c53fe12fc63597d1cdb80d0a2548f0e3ae
janejane <janetjanedchic@yahoo.com>
2021-03-15 20:36:58 -0400

    Setting up staff backends

--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from cart.models import Order
+from .mixins import StaffUserMixin
 
-# Create your views here.
+
+class StaffView(LoginRequiredMixin, StaffUserMixin,  generic.ListView):
+    template_name = 'staff/staff.html'
+    queryset = Order.objects.filter(ordered=True).order_by('-ordered_date')
+    paginate_by = 20
+    context_object_name = 'orders'
