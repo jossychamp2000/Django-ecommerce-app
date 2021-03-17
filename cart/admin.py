34f462011ac45e2ff846cd74f1a659203383b5a9
janejane <janetjanedchic@yahoo.com>
2021-03-17 13:08:51 -0400

    Added category into Model,ProductList-view & HTML

--- cart/admin.py
+++ cart/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import (Product, OrderItem, Order,
-                     ColourVariation, SizeVariation, Address, Payment)
+                     ColourVariation, SizeVariation, Address, Payment, Category)
 
 
 class AddressAdmin(admin.ModelAdmin):
@@ -14,6 +14,7 @@ class AddressAdmin(admin.ModelAdmin):
     ]
 
 
+admin.site.register(Category)
 admin.site.register(Address)
 admin.site.register(Product)
 admin.site.register(OrderItem)
